eab3e9bd6b30e8f1017dcc0888856a866e1d3168
froliceric914 <wzhao.eric@gmail.com>
2018-09-19 19:12:50 +0000

    Adds functions exercises

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num) {
+   return num % 2 !== 0;
+}
+
+
+var threeIsOdd = isOdd(3);
+var eightIsOdd = isOdd(8);
+
+console.log("3 is odd: " + threeIsOdd);
+console.log("8 is odd: " + eightIsOdd)
